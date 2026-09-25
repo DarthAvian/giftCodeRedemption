238a6d19cf2ca4e4dbda8eb90d46adcbf76042e8
DarthAvian <cjsbird1@gmail.com>
2026-09-25 10:45:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import streamlit as st
 
 # Google Sheet CSV URL
-SHEET_ID = "1oMpMuS_lrDgQSFlpXNcVCpclb"
+SHEET_ID = "1oMpMuS_lrDgQSFlpXNcVCpclbKz7gIq1lqtzXomGMAis"
 GID = "0"
 SHEET_CSV_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv&gid={GID}"
 
